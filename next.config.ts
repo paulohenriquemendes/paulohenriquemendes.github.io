0163894eb5a62ae187d04b3ca2fe05f756110739
paulohenriquemendes <p-h-m@outlook.com>
2024-12-28 16:18:25 -0300

    iniciando app

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",  // <=== habilita o export estático
+  reactStrictMode: true,
   /* config options here */
 };
+
+module.exports = {
+  assetPrefix: './',
+  basePath: '/paulohenriquemendes',
+  trailingSlash: true,
+}
+
+module.exports = nextConfig;
 
 export default nextConfig;
